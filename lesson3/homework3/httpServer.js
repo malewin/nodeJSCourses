54913f2f5071ccd199ec83df0ba2c2d4413255c9
malewin <139165145+malewin@users.noreply.github.com>
2024-10-16 22:21:33 +0600

    hw3 is completed. Express server is ready

---
+++ lesson3/homework3/httpServer.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const fs = require('fs').promises;
+const path = require('path');
+
+const pathForWriteDB = path.resolve(__dirname, 'counters.json');
+
+// Проверка наличия файла и его создание
+async function initializeCounters() {
+    try {
+        await fs.access(pathForWriteDB);
+    } catch {
+        const counters = {
+            '/': 0,
+            '/about': 0
+        };
+        await fs.writeFile(pathForWriteDB, JSON.stringify(counters, null, 4));
+    }
+}
+
+// Промежуточный обработчик для инкрементации счётчика
+app.use(async(req, res, next) => {
+    try {
+        const data = await fs.readFile(pathForWriteDB);
+        const counters = JSON.parse(data);
+
+        // Обновляем счётчик для текущего маршрута
+        if (counters[req.path] !== undefined) {
+            counters[req.path] += 1;
+            await fs.writeFile(pathForWriteDB, JSON.stringify(counters, null, 4));
+        }
+    } catch (err) {
+        console.error(err);
+    }
+    next();
+});
+
+// Корневая страница
+app.get('/', async(req, res) => {
+    const data = await fs.readFile(pathForWriteDB);
+    const counters = JSON.parse(data);
+    res.send(`<h1>Корневая страница</h1><br><p>Количество просмотров: ${counters['/']}</p><a href='/about'>Ссылка на страницу about</a>`);
+});
+
+// Страница about
+app.get('/about', async(req, res) => {
+    const data = await fs.readFile(pathForWriteDB);
+    const counters = JSON.parse(data);
+    res.send(`<h1>Страница about</h1><br><p>Количество просмотров: ${counters['/about']}</p><a href='/'>Ссылка на Корневую страницу</a>`);
+});
+
+const port = 3000;
+
+initializeCounters().then(() => {
+    app.listen(port, () => {
+        console.log(`Сервер запущен на порту ${port}`);
+    });
+});
